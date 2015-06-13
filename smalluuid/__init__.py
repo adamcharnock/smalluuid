dff9b84904ad1bcd7b04eba2595ba292485cb675
Adam Charnock <adam@omniwiki.co.uk>
2015-06-13 12:54:40 +0100

    Fixing indentation

--- smalluuid/__init__.py
+++ smalluuid/__init__.py
@@ -1,15 +1,14 @@
 from __future__ import absolute_import
- 
- # It must be possible to import this file with 
- # none of the package's dependencies installed
- 
- __version__ = '0.1.0'
 
- DEFAULT_ALPHABET = tuple("23456789ABCDEFGHJKLMNPQRSTUVWXYZ"
-                          "abcdefghijkmnopqrstuvwxyz")
- 
- from smalluuid.smalluuid import (
-     SmallUUID,
-     TypedSmallUUID,
- )
- 
+# It must be possible to import this file with
+# none of the package's dependencies installed
+
+__version__ = '0.1.0'
+
+DEFAULT_ALPHABET = tuple("23456789ABCDEFGHJKLMNPQRSTUVWXYZ"
+                      "abcdefghijkmnopqrstuvwxyz")
+
+from smalluuid.smalluuid import (
+ SmallUUID,
+ TypedSmallUUID,
+)
