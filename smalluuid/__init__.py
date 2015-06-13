9affbfdc6621c9318a4ee10828811a33c87d47d2
Adam Charnock <adam@omniwiki.co.uk>
2015-06-13 12:49:26 +0100

    Version bump to 0.1.0 and updating CHANGES.txt

--- smalluuid/__init__.py
+++ smalluuid/__init__.py
@@ -1,14 +1,15 @@
 from __future__ import absolute_import
+ 
+ # It must be possible to import this file with 
+ # none of the package's dependencies installed
+ 
+ __version__ = '0.1.0'
 
-# It must be possible to import this file with 
-# none of the package's dependencies installed
-
-__version__ = '0.1.0'
-
-DEFAULT_ALPHABET = tuple("23456789ABCDEFGHJKLMNPQRSTUVWXYZ"
-                         "abcdefghijkmnopqrstuvwxyz")
-
-from smalluuid.smalluuid import (
-    SmallUUID,
-    TypedSmallUUID,
-)
+ DEFAULT_ALPHABET = tuple("23456789ABCDEFGHJKLMNPQRSTUVWXYZ"
+                          "abcdefghijkmnopqrstuvwxyz")
+ 
+ from smalluuid.smalluuid import (
+     SmallUUID,
+     TypedSmallUUID,
+ )
+ 
